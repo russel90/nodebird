81ccd834c9f50bc303c760bec14b00472094ae03
russel <Yongwan.Kim90@gmail.com>
2019-05-17 11:10:08 +0900

    Add Upload/Search Function

--- passport/index.js
+++ passport/index.js
@@ -3,16 +3,26 @@ const kakao = require("./kakaoStrategy");
 const { User } = require("../models");
 
 module.exports = passport => {
-  // req.session 객체에 어떤 데이터를 저장할지 선택 함.
   passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  // 매 요청시 실행
-  // 세션에 저장했던 아이드를 받아사용자 정보 조회
-  // 조회한 정보를 req.user에저장하여 req.user를 통해 로그인한 사용자의 정보를 가져올 수 있음
   passport.deserializeUser((id, done) => {
-    User.findOne({ where: { id } })
+    User.findOne({
+      where: { id },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nick"],
+          as: "Followers"
+        },
+        {
+          model: User,
+          attributes: ["id", "nick"],
+          as: "Followings"
+        }
+      ]
+    })
       .then(user => done(null, user))
       .catch(err => done(err));
   });
